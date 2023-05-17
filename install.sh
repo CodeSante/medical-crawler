ad455114b027fd147021af973b3e538cfe26bab2
theophpo <theoph.po@gmail.com>
2023-05-17 12:52:34 +0200

    Requirements

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 wget https://raw.githubusercontent.com/CodeSante/medical-wordlist/main/fr/wordlist.fr.txt
 mv wordlist.fr.txt keywords.txt
 sudo apt install libpq-dev
-pip3 install bs4
-pip3 install scrapy
-pip3 install unidecode
-pip3 install psycopg2
+pip install -r requirements.txt
 bash scripts/install-db.sh
